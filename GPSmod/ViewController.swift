d78c1cff6bb6ea668f5798e11f0b2e410d439b2a
Mateusz Jur <mateusz.jur92@gmail.com>
2019-08-12 18:14:26 +0200

    first commit gpsmod

---
+++ GPSmod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPSmod
+//
+//  Created by Mateusz Jur on 12/08/2019.
+//  Copyright © 2019 Mateusz Jur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
